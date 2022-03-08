63a9936cf7d59d39a995a9b1e0c41f44c84c0743
ish-04 <msviji2001@gmail.com>
2022-03-08 12:26:38 +0530

    Create logger.py

---
+++ Milestone1/logger.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+import logging
+
+logging.basicConfig(filename="Milestone1_Log.txt", format='%(message)s',filemode='w')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+def log(message):
+    logger.info(f'{datetime.now()};{message}')
